a2d77d13b59d00dee8b0305ed6b1fa26b0ae9376
Melih DİNÇER <m_dincer41@hotmail.com>
2024-02-25 23:26:40 +0300

    BootcampImageConfiguration dosyasının eklenmesi.

---
+++ DataAccess/Concretes/EntityFramework/EntityTypeConfigurations/BootcampImageConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Concretes.EntityFramework.EntityTypeConfigurations
+{
+    public class BootcampImageConfiguration : IEntityTypeConfiguration<BootcampImage>
+    {
+        public void Configure(EntityTypeBuilder<BootcampImage> builder)
+        {
+            builder.ToTable("BootcampImages").HasKey("Id");
+            builder.Property(x => x.Id).HasColumnName("Id");
+            builder.Property(x => x.BootcampId).HasColumnName("BootcampId").IsRequired();
+            builder.Property(x => x.ImagePath).HasColumnName("ImagePath").IsRequired();
+
+            builder.HasOne(x => x.Bootcamp);
+        }
+    }
+}
