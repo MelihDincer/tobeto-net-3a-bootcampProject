3d53af8f82fd676de711fd2e5f9dfe56e22cee01
Melih DİNÇER <m_dincer41@hotmail.com>
2024-02-26 22:36:53 +0300

    BlackList için MappingProfiles oluşturuldu.

---
+++ Business/Profiles/BlackLists/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Business.Requests.BlackLists;
+using Business.Responses.BlackLists;
+using Entities.Concretes;
+
+namespace Business.Profiles.BlackLists
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<BlackList, CreateBlackListRequest>().ReverseMap();
+            CreateMap<BlackList, DeleteBlackListRequest>().ReverseMap();
+            CreateMap<BlackList, UpdateBlackListRequest>().ReverseMap();
+
+            CreateMap<BlackList, CreateBlackListResponse>().ReverseMap();
+            CreateMap<BlackList, UpdateBlackListResponse>().ReverseMap();
+            CreateMap<BlackList, GetAllBlackListResponse>().ReverseMap();
+            CreateMap<BlackList, GetByIdBlackListResponse>().ReverseMap();
+        }
+    }
+}
