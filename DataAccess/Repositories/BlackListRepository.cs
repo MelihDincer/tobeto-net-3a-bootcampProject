cc7e16e2e5d5226c42c1c9b1f1a4b05dedd2b1bc
Melih DİNÇER <m_dincer41@hotmail.com>
2024-02-26 21:24:33 +0300

    BlackListRepository'nin oluşturulması.

---
+++ DataAccess/Repositories/BlackListRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstracts;
+using DataAccess.Concretes.EntityFramework.Contexts;
+using Entities.Concretes;
+
+namespace DataAccess.Repositories
+{
+    public class BlackListRepository : EfRepositoryBase<BlackList, int, BaseDbContext>, IBlackListRepository
+    {
+        public BlackListRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
