ce99196dd2c61ccc10a3b88e9edce2833db85fe0
Melih DİNÇER <m_dincer41@hotmail.com>
2024-02-26 22:38:35 +0300

    BlackLists Api Controller'ın oluşturulması ve Api testlerinin başarıyla tamamlanması.

---
+++ WebAPI/Controllers/BlackListsController.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstracts;
+using Business.Requests.BlackLists;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlackListsController : BaseController
+    {
+        private readonly IBlackListService _blackListService;
+
+        public BlackListsController(IBlackListService blackListService)
+        {
+            _blackListService = blackListService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return HandleDataResult(await _blackListService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            return HandleDataResult(await _blackListService.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(CreateBlackListRequest request)
+        {
+            return HandleDataResult(await _blackListService.CreateAsync(request));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(DeleteBlackListRequest request)
+        {
+            return HandleResult(await _blackListService.DeleteAsync(request));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(UpdateBlackListRequest request)
+        {
+            return HandleDataResult(await _blackListService.UpdateAsync(request));
+        }
+    }
+}
