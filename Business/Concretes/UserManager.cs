dcf1a4cd2c0a7956281b125cc028bc53ff778a97
Melih DİNÇER <m_dincer41@hotmail.com>
2024-02-28 23:08:36 +0300

    UserManager için business rules uygulanması.

--- Business/Concretes/UserManager.cs
+++ Business/Concretes/UserManager.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Business.Abstracts;
 using Business.Responses.Users;
+using Core.Exceptions.Types;
 using Core.Utilities.Results;
 using DataAccess.Abstracts;
+using DataAccess.Repositories;
 using Entities.Concretes;
 
 namespace Business.Concretes
@@ -26,9 +28,17 @@ namespace Business.Concretes
 
         public async Task<IDataResult<GetByIdUserResponse>> GetByIdAsync(int id)
         {
+            await CheckIfUserNotExists(id);
             User user = await _userRepository.GetAsync(x => x.Id == id);
             GetByIdUserResponse response = _mapper.Map<GetByIdUserResponse>(user);
             return new SuccessDataResult<GetByIdUserResponse>(response);
+        }
+
+        private async Task CheckIfUserNotExists(int userId)
+        {
+            var isExists = await _userRepository.GetAsync(u => u.Id == userId);
+            if (isExists is null)
+                throw new BusinessException("User does not exists");
         }
     }
 }
