41c058dbad3b91684785cd0f9e311a861e59f179
Melih DİNÇER <m_dincer41@hotmail.com>
2024-03-10 23:09:21 +0300

    UserOperationClaim entity sınıfı eklendi.

---
+++ Core/Utilities/Security/Entities/UserOperationClaim.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+
+namespace Core.Utilities.Security.Entities;
+
+public class UserOperationClaim : BaseEntity<int>
+{
+    public int UserId { get; set; }  
+    public int OperationClaimId { get; set; }
+    public virtual User User { get; set; }
+    public virtual OperationClaim OperationClaim { get; set; }
+
+    public UserOperationClaim()
+    {
+
+    }
+
+    public UserOperationClaim(int id, int userId, int operationClaimId) : this()
+    {
+        Id = id;
+        UserId = userId;
+        OperationClaimId = operationClaimId;
+    }
+
+    public UserOperationClaim(int userId, int operationClaimId) : this()
+    {
+        UserId = userId;
+        OperationClaimId = operationClaimId;
+    }
+}
