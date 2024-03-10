3b820f06019699da01568532a466b4bf077310f8
Melih DİNÇER <m_dincer41@hotmail.com>
2024-03-10 23:08:52 +0300

    User sınıfı revize edildi.

--- Core/Utilities/Security/Entities/User.cs
+++ Core/Utilities/Security/Entities/User.cs
@@ -2,12 +2,47 @@
 
 namespace Core.Utilities.Security.Entities;
 
-public class User:BaseEntity<int>
+public class User : BaseEntity<int>
 {
+    public string UserName { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string NationalIdentity { get; set; }
     public string Email { get; set; }
-    public byte[] PasswordHash {  get; set; }
+    public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public virtual ICollection<UserOperationClaim> UserOperationClaims { get; set; }
 
+    public User()
+    {
+        UserOperationClaims = new HashSet<UserOperationClaim>();
+    }
+
+    public User(int id, string userName, string firstName, string lastName, DateTime dateOfBirth, string nationalIdentity,
+            string email, byte[] passwordHash, byte[] passwordSalt) : this()
+    {
+        Id = id;
+        UserName = userName;
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dateOfBirth;
+        NationalIdentity = nationalIdentity;
+        Email = email;
+        PasswordHash = passwordHash;
+        PasswordSalt = passwordSalt;
+    }
+
+    public User(string userName, string firstName, string lastName, DateTime dateOfBirth, string nationalIdentity,
+            string email, byte[] passwordHash, byte[] passwordSalt) : this()
+    {
+        UserName = userName;
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dateOfBirth;
+        NationalIdentity = nationalIdentity;
+        Email = email;
+        PasswordHash = passwordHash;
+        PasswordSalt = passwordSalt;
+    }
 }
